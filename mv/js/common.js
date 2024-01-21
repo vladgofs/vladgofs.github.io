8d5eabfaa944039907355d7a044f3bf4f2853091
vladgofs <kojukhar.vladislav@yandex.ru>
2024-01-21 18:17:13 +0500

    update

--- mv/js/common.js
+++ mv/js/common.js
@@ -1,11 +1,20 @@
 	$(function() { 
 		
 		$('.banner').unslider({ 
-			autoplay: true, 
+			autoplay: true,
+			keys: false, 
 			delay: 10000,
 			nav: false,
 			infinite: true 
 		});
+
+		$('.content-slide').unslider({ 
+			autoplay: true,
+			delay: 20000,
+			nav: false,
+			infinite: true 
+		});
+
 
 
 
